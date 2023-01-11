38e12f6ec61f8ba7caf11dde0a57af3fed2b82af
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-01-11 10:42:55 +0200

    delete React.Fragment brackets

--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -14,33 +14,32 @@ type Genre = {
 function Discover() {
   const { data, isFetching, error } = useGetTopChartsQuery(null);
 
-  if(isFetching) {
-    return <Loader title="Load to songs..."/>
+  if (isFetching) {
+    return <Loader title="Loading songs..." />;
   }
 
-  if(error) {
-    return <Error/>
+  if (error) {
+    return <Error />;
   }
 
   return (
-      <div className="flex flex-col">
-        <div className="w-full flex flex-col justify-between items-center mt-4 mb-10 sm:flex-row">
-          <h2 className="font-bold text-3xl text-white text-left">Discover</h2>
-          <select className="bg-black text-gray-300 p-3  text-sm rounded-lg outline-none mt-0 sm:mt-5">
-            {genres.map((genre: Genre) => (
-              <option key={genre.title} value={genre.value}>
-                {genre.title}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="flex flex-wrap justify-center gap-8 sm:justify-start sm:gap-5">
-          {data?.map((song) => (
-            <SongCard key={song.key}>Song Card</SongCard>
+    <div className="flex flex-col">
+      <div className="w-full flex flex-col justify-between items-center mt-4 mb-10 sm:flex-row">
+        <h2 className="font-bold text-3xl text-white text-left">Discover</h2>
+        <select className="bg-black text-gray-300 p-3  text-sm rounded-lg outline-none mt-0 sm:mt-5">
+          {genres.map((genre: Genre) => (
+            <option key={genre.title} value={genre.value}>
+              {genre.title}
+            </option>
           ))}
-        </div>
+        </select>
       </div>
-    </>
+      <div className="flex flex-wrap justify-center gap-8 sm:justify-start sm:gap-5">
+        {data?.map((song) => (
+          <SongCard key={song.key}>Song Card</SongCard>
+        ))}
+      </div>
+    </div>
   );
 }
 
