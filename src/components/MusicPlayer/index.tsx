8d083ca8c1d5bc78305b5a452c35de58b028d3dd
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-01-12 11:59:52 +0200

    done with Track component

--- src/components/MusicPlayer/index.tsx
+++ src/components/MusicPlayer/index.tsx
@@ -1,7 +1,20 @@
+import { useSelector } from "react-redux";
+import { SelectorPlayerState } from "../../API/types";
+import Track from "./Track";
+
 function MusicPlayer() {
+    const {isActive, isPlaying, activeSong, currentSongs, currentIndex} = useSelector((state:SelectorPlayerState) => state.player);
     return (
       <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
-        
+        <div className="relative w-full flex items-center justify-between sm:px-12 px-8">
+          <Track isActive={isActive} isPlaying={isPlaying} activeSong={activeSong}/>
+          <div className="flex flex-1 flex-col items-center justify-center">
+            <div className="controls"></div>
+            <div className="seekbar"></div>
+            <div className="player"></div>
+          </div>
+          <div className="volume"></div>
+        </div>
       </div>
     );
 }
