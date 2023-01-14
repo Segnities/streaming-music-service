1d924ed33522ed67f618ad2cd667539861899b38
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-01-14 09:56:53 +0200

    Seekbar component is done

--- src/components/MusicPlayer/index.tsx
+++ src/components/MusicPlayer/index.tsx
@@ -7,13 +7,21 @@ import Track from "./Track";
 import { playPause, nextSong, prevSong } from "../../store/reducers/player";
 
 import { SelectorPlayerState } from "../../API/types";
+import Seekbar from "./Seekbar";
 
 function MusicPlayer() {
   const { isActive, isPlaying, activeSong, currentSongs, currentIndex } =
     useSelector((state: SelectorPlayerState) => state.player);
-  const dispatch = useDispatch();
   const [repeat, setRepeat] = useState(false);
   const [shuffle, setShuffle] = useState(false);
+  const [seekTime, setSeekTime] = useState(0);
+  const [appTime, setAppTime] = useState(0);
+  const [volume, setVolume] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  const dispatch = useDispatch();
+
+
 
   const handlePlayPause = () => {
     if (!isActive) {
@@ -47,6 +55,10 @@ function MusicPlayer() {
     }
   };
 
+  const onInput = (event) => {
+    setSeekTime(event.target.value);
+  }
+
   return (
     <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
       <div className="relative w-full flex items-center justify-between sm:px-12 px-8">
@@ -68,7 +80,14 @@ function MusicPlayer() {
             handlePrevSong={handlePrevSong}
             handleNextSong={handleNextSong}
           />
-          <div className="seekbar"></div>
+          <Seekbar
+            value={appTime}
+            min={0}
+            max={duration}
+            onInput={onInput}
+            setSeekTime={setSeekTime}
+            appTime={appTime}
+          />
           <div className="player"></div>
         </div>
         <div className="volume"></div>
