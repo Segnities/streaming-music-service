4b1c0a6dbb754b125da0318b53f2cd81ea06aa9b
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-01-18 11:35:16 +0200

    done with loading data problem in topCharts

--- src/components/TopCharts.tsx
+++ src/components/TopCharts.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 
@@ -25,20 +25,23 @@ function TopChartCard(props: TopChartCardProps) {
   const { song, index } = props;
   return (
     <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
-      {song.title}
+      <h3 className="font-bold text-base text-white mr-3">
+        {index + 1}. {song.title}
+      </h3>
+      <div className="flex flex-1 flex-row justify-between items-center"></div>
     </div>
   );
 }
 
 function TopCharts() {
-  const dispatch = useDispatch();
+  const { data, isFetching } = useGetTopChartsQuery(null);
+  const topCharts = isFetching ? [] : [...data].slice(0, 5);
   const { activeSong, isPlaying } = useSelector(
     (state: SelectorPlayerState) => state.player
   );
   const containerRef = useRef(null);
-  const { data } = useGetTopChartsQuery(null);
 
-  const topCharts = data.map((item) => item).slice(0, 5);
+  const dispatch = useDispatch();
 
   const handlePlayClick = (song: RootObject, index: number) => {
     dispatch(setActiveSong({ song, data, index }));
@@ -68,9 +71,11 @@ function TopCharts() {
           </Link>
         </div>
         <div className="flex flex-col gap-1 mt-4">
-          {topCharts?.map((song, index) => (
-            <TopChartCard key={song.key} song={song} index={index} />
-          ))}
+          {topCharts
+            .map((song, index) => (
+              <TopChartCard key={song?.key} song={song} index={index} />
+            ))
+            .slice(0, 5)}
         </div>
       </div>
 
@@ -84,22 +89,30 @@ function TopCharts() {
           </Link>
         </div>
 
-      <Swiper
-        slidesPerView={'auto'}
-        spaceBetween={15}
-        freeMode
-        centeredSlides
-        centeredSlidesBounds
-        modules={[FreeMode]}
-        className="mt-4"
-      >
-        {
-          topCharts?.map((song, index) => (
-            <SwiperSlide key={song.key} className="w-[25%] h-auto shadow-lg rounded-full animate-slideright"></SwiperSlide>
-          ))
-        }
-      </Swiper>
-
+        <Swiper
+          slidesPerView={"auto"}
+          spaceBetween={15}
+          freeMode
+          centeredSlides
+          centeredSlidesBounds
+          modules={[FreeMode]}
+          className="mt-4"
+        >
+          {topCharts.map((song: RootObject, index) => (
+            <SwiperSlide
+              key={song?.key}
+              className="w-1/5 h-1/5 shadow-lg rounded-full animate-slideright"
+            >
+              <Link to={`artists/${song?.artists[0]?.adamid}`}>
+                <img
+                  src={song?.images?.background}
+                  alt="artist_back"
+                  className="rounded-full w-full object-cover"
+                />
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
