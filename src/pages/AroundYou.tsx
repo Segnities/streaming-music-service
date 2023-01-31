8e9d9aa411814db4535f4a6b91866f7779bb15e4
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-01-31 16:45:14 +0200

    Geo Ipify API
    The geolocation is working!

--- src/pages/AroundYou.tsx
+++ src/pages/AroundYou.tsx
@@ -4,17 +4,26 @@ import { useSelector } from "react-redux";
 import Loader from "../components/UI/Loader";
 import Error from "../components/UI/Error";
 
-import { SelectorPlayerState } from "../API/types";
+import { GeoIpifyCountry, SelectorPlayerState } from "../API/types";
+import { GeoIpify } from "../API/geoIpify";
 
 function AroundYou() {
   const [country, setCountry] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const { activeSong, isPlaying } = useSelector(
     (state: SelectorPlayerState) => state.player
   );
 
-  useEffect(() => {}, [country]);
+  console.log(country);
+
+  useEffect(() => {
+    GeoIpify.getGeoIpifyData()
+      .then((res: GeoIpifyCountry) => setCountry(res?.data?.location?.country))
+      .catch((error) => setError(error.message))
+      .finally(() => setIsLoading(true));
+  }, [country]);
 
   return <div>Around you</div>;
 }
