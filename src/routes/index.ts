25c1ebce89a470d2a5c2807712fc2a8741f6d819
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-02-04 11:23:26 +0200

    User private user link
    I forgot to add user link + icon in private links

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -15,6 +15,7 @@ import {
   HiOutlinePhotograph,
   HiOutlineUserGroup,
   HiOutlineHashtag,
+  HiUser,
 } from "react-icons/hi";
 
 type Routing = {
@@ -24,25 +25,41 @@ type Routing = {
 }[];
 
 export const publicRoutes: Routing = [
+  { id: "/", path: "/", element: Discover },
   { id: "/login", path: "/login", element: Login },
   { id: "/sign-up", path: "/sign-up", element: SignUp },
-  { id: "/", path: "/", element: Discover },
   { id: "/top-charts", path: "/top-charts", element: TopCharts },
   { id: "/top-artists", path: "/top-artists", element: TopArtists },
   { id: "/around-you", path: "/around-you", element: AroundYou },
-  { id: "*", path: "*", element: NotFound },
   { id: "/artists/:id", path: "/artists/:id", element: Artist },
   { id: "/songs/:songid", path: "/songs/:songid", element: Song },
   { id: "/search/:query", path: "/search/:query", element: Search },
+  { id: "*", path: "*", element: NotFound },
 ];
 
 export const privateRoutes: Routing = [
+  { id: "/", path: "/", element: Discover },
   { id: "/user", path: "/user", element: User },
+  { id: "/top-charts", path: "/top-charts", element: TopCharts },
+  { id: "/top-artists", path: "/top-artists", element: TopArtists },
+  { id: "/around-you", path: "/around-you", element: AroundYou },
+  { id: "/artists/:id", path: "/artists/:id", element: Artist },
+  { id: "/songs/:songid", path: "/songs/:songid", element: Song },
+  { id: "/search/:query", path: "/search/:query", element: Search },
+  { id: "*", path: "*", element: NotFound },
 ];
 
-export const links = [
+export const publicLinks = [
   { name: "Discover", to: "/", icon: HiOutlineHome },
   { name: "Around You", to: "/around-you", icon: HiOutlinePhotograph },
   { name: "Top Artists", to: "/top-artists", icon: HiOutlineUserGroup },
   { name: "Top Charts", to: "/top-charts", icon: HiOutlineHashtag },
+];
+
+export const privateLinks = [
+  { name: "Discover", to: "/", icon: HiOutlineHome },
+  { name: "Around You", to: "/around-you", icon: HiOutlinePhotograph },
+  { name: "Top Artists", to: "/top-artists", icon: HiOutlineUserGroup },
+  { name: "Top Charts", to: "/top-charts", icon: HiOutlineHashtag },
+  { name: "User", to: "/user", icon: HiUser },
 ];
