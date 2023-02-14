19ca24e0b16a3faad023340eac96cf1cbfcd9e4d
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-02-14 21:02:39 +0200

    getUsers util, Login remember functionality
    getUsers util can upload data from firestore users table. Login remember functionalty is created by setPersistence firebase method.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,17 @@
-import { NavLink } from "react-router-dom";
-import { useFormik } from "formik";
+import { useContext } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+
+import { Formik, Form, Field } from "formik";
 
 import * as Yup from "yup";
 
 import { FcGoogle } from "react-icons/fc";
 import { AiFillFacebook, AiOutlineTwitter } from "react-icons/ai";
-import { useContext, useEffect, useState } from "react";
+
+
 import { AuthContext } from "../context";
+import { browserLocalPersistence, browserSessionPersistence, getAuth, setPersistence, signInWithEmailAndPassword } from "firebase/auth";
+import { firebaseApp } from "../firebase/firebaseConfig";
 
 
 const validationSchema = Yup.object().shape({
@@ -14,36 +19,28 @@ const validationSchema = Yup.object().shape({
     password: Yup.string().min(1).required('Password is required'),
     rememberMe: Yup.boolean()
 
-})
+});
 
 function Login() {
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: '',
-            rememberMe: false,
-        },
-        validationSchema: validationSchema,
-        onSubmit(values, formikHelpers) {
-            alert(JSON.stringify(values, null, 2))
-        },
-    });
-    const [successSignUp, setSuccessSignUp] = useState(false);
-
+    const auth = getAuth(firebaseApp);
     const authContext = useContext(AuthContext);
 
-    useEffect(() => {
-        if (localStorage.getItem("success-user-sign-up") === "true") {
-            setSuccessSignUp(true);
-        }
-    })
+    const navigate = useNavigate();
 
+    const signIn = async (email: string, password: string, rememberMe: boolean) => {
+        await setPersistence(auth, rememberMe ? browserLocalPersistence : browserSessionPersistence);
+
+        signInWithEmailAndPassword(auth, email, password).then(
+            (res) => {
+                authContext?.setIsAuth(true);
+                authContext?.setUser(res?.user);
+                navigate('/');
+            }).catch(err => alert(err.code + ":" + err.message));
+
+    }
 
     return (
         <div className="flex flex-1 flex-col items-center">
-            {
-                successSignUp && <div className="w-full p-5 font-bold text-2xl color-white bg-green-600">Your successfully sign up to Vite!</div>
-            }
             <p className="text-base font-bold">To continue, log to Vite.</p>
             <div className="flex flex-col flex-1 mt-2">
                 <div>
@@ -66,45 +63,55 @@ function Login() {
                     <hr role="presentation" className="flex-1 border-[1px] border-solid border-gray-300" />
                 </div>
                 <div className="flex flex-col flex-1 mt-4">
-                    <form onSubmit={formik.handleSubmit} autoComplete="off">
-                        <div className="flex flex-col">
-                            <label htmlFor="email" className="text-sm font-bold my-1">Email address or username</label>
-                            <input
-                                type="text"
-                                value={formik.values.email}
-                                onChange={formik.handleChange}
-                                id="email"
-                                autoComplete="off"
-                                name="email"
-                                placeholder="Email address or username"
-                                className="text-base normal-case my-1 line tracking-normal p-3 border-[1px]  focus-visible:border-[3px] border-gray-800"
-                            />
-                        </div>
-                        <div className="flex flex-col">
-                            <label htmlFor="password" className="text-sm font-bold my-1">Password</label>
-                            <input
-                                type="password"
-                                name="password"
-                                value={formik.values.password}
-                                onChange={formik.handleChange}
-                                id="password"
-                                placeholder="Password"
-                                className="text-base normal-case line my-1 tracking-normal p-3 border-[1px] focus-visible:border-[3px] border-gray-800"
-                            />
-                        </div>
-                        <p className="mt-2">
-                            <NavLink to='/forgot-password' className="underline text-base font-semibold hover:text-[#1ED760]"> Forgot your password?</NavLink>
-                        </p>
-                        <div className="flex justify-between items-center mt-3">
-                            <div className="flex flex-row-reverse items-center">
-                                <label htmlFor="remember-me" className="font-medium text-base ml-4">Remember me</label>
-                                <input type="checkbox" name="rememberMe" checked={formik.values.rememberMe} onChange={formik.handleChange} className="w-4 h-4" id="remember-me" />
-                            </div>
-                            <div>
-                                <button type="submit" className="bg-[#1ED760] w-32 text-sm p-4 rounded-3xl text-black font-medium">LOG IN</button>
-                            </div>
-                        </div>
-                    </form>
+                    <Formik
+                        initialValues={{
+                            email: "",
+                            password: "",
+                            rememberMe: false
+                        }}
+                        validationSchema={validationSchema}
+                        onSubmit={(values) => { signIn(values.email, values.password, values.rememberMe) }}
+                    >
+                        {
+                            ({ errors, touched, values }) => (
+                                <Form autoComplete="off">
+                                    <div className="flex flex-col">
+                                        <label htmlFor="email" className="text-sm font-bold my-1">Email address</label>
+                                        <Field
+                                            type="text"
+                                            id="email"
+                                            autoComplete="off"
+                                            name="email"
+                                            placeholder="Enter your email address"
+                                            className="text-base normal-case my-1 line tracking-normal p-3 border-[1px]  focus-visible:border-[3px] border-gray-800"
+                                        />
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <label htmlFor="password" className="text-sm font-bold my-1">Password</label>
+                                        <Field
+                                            type="password"
+                                            name="password"
+                                            id="password"
+                                            placeholder="Password"
+                                            className="text-base normal-case line my-1 tracking-normal p-3 border-[1px] focus-visible:border-[3px] border-gray-800"
+                                        />
+                                    </div>
+                                    <p className="mt-2">
+                                        <NavLink to='/forgot-password' className="underline text-base font-semibold hover:text-[#1ED760]"> Forgot your password?</NavLink>
+                                    </p>
+                                    <div className="flex justify-between items-center mt-3">
+                                        <div className="flex flex-row-reverse items-center">
+                                            <label htmlFor="remember-me" className="font-medium text-base ml-4">Remember me</label>
+                                            <Field type="checkbox" name="rememberMe" className="w-4 h-4" id="remember-me" />
+                                        </div>
+                                        <div>
+                                            <button type="submit" className="bg-[#1ED760] w-32 text-sm p-4 rounded-3xl text-black font-medium">LOG IN</button>
+                                        </div>
+                                    </div>
+                                </Form>
+                            )
+                        }
+                    </Formik>
                     <hr />
                     <div className="flex flex-col mt-4">
                         <p className="text-center font-bold text-lg">Don't have an account?</p>
