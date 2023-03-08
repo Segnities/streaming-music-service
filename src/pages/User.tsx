02c0677368ebab173cade3acccdffc4b2b4229c8
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-03-08 21:35:49 +0200

    Approved && done with basic UI of User page

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -9,7 +9,6 @@ import { firebaseApp, firebaseDatabase } from "../firebase/firebaseConfig";
 
 import { getUser } from "../utils/getUser";
 
-import { AiOutlinePoweroff } from "react-icons/ai";
 import { UserDoc } from "../utils/@types";
 
 import NoImage from "../assets/no_artist.jpg";
@@ -51,46 +50,57 @@ function User() {
         return <Loader />
     }
 
-
     return (
-        <div className="flex flex-col">
-            <div className="relative w-full flex flex-col">
+        <div className="flex flex-col w-full">
+            <h1 className="text-2xl sm:text-3xl text-white font-semibold my-5">Account overview</h1>
+            <article className="relative w-full flex flex-col">
+                <h2 className="text-xl sm:text-2xl text-white font-medium my-4">Profile</h2>
                 <div className="w-full h-28 bg-gradient-to-l from-transparent to-black sm:h-52"></div>
-                <div className="absolute inset-0 flex flex-row items-center">
+                <section className="absolute inset-0 flex flex-row items-center">
                     <img
                         src={authContext?.user?.photoURL ?? NoImage}
                         alt="art"
                         className="w-28 sm:w-48 h-28 sm:h-48 rounded-full object-cover border-2 shadow-xl shadow-black cursor-pointer"
                         onDragStart={(e) => e.preventDefault()}
+                        title="Click to edit profile"
                     />
 
-                    <div className="ml-5 mb-3">
-                        <p className="text-3xl sm:text-xl md:text-2xl text-white mb-2">
-                            {authContext?.user?.displayName ?? firebaseUser?.data?.username}
-                        </p>
-                    </div>
-                </div>
+                    <h3 className="text-3xl sm:text-xl md:text-2xl text-white mb-5 ml-5">
+                        {authContext?.user?.displayName ?? firebaseUser?.data?.username}
+                    </h3>
+                </section>
                 <div className="w-full h-24 sm:h-24" />
+            </article>
+            <div className="flex flex-col">
+                <table className="w-full">
+                    <colgroup>
+                        <col className="w-1/2" />
+                        <col className="w-1/2" />
+                    </colgroup>
+                    <tbody>
+                        <tr className="border-b-2 border-[#dedede]">
+                            <td className="py-5 text-gray-500 text-lg">Username</td>
+                            <td className="py-5 text-white text-lg">{firebaseUser?.data?.username ?? authContext?.user?.displayName}</td>
+                        </tr>
+                        <tr className="border-b-2 border-[#dedede]">
+                            <td className="py-5 text-gray-500 text-lg">Email</td>
+                            <td className="py-5 text-white text-lg">{firebaseUser?.data?.email ?? authContext?.user?.email}</td>
+                        </tr>
+                        <tr className="border-b-2 border-[#dedede]">
+                            <td className="py-5 text-gray-500 text-lg">Date of birth</td>
+                            <td className="py-5 text-white text-lg">{firebaseUser?.data?.birthday || "Unknown"}</td>
+                        </tr>
+                        <tr className="border-b-2 border-[#dedede]">
+                            <td className="py-5 text-gray-500 text-lg">Gender</td>
+                            <td className="py-5 text-white text-lg">{firebaseUser?.data?.gender || "Unknown"}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-            <div className="mt-3 w-full">
-                <div>
-                    <p className="text-white text-2xl my-2">Email: <span className="text-white italic text-lg">{firebaseUser?.data?.email}</span></p>
-                </div>
-                {firebaseUser?.data.birthday && <div>
-                    <p className="text-white text-2xl my-2">Birthday: <span className="text-white italic text-lg">{firebaseUser?.data?.birthday}</span></p>
-                </div>}
-                {firebaseUser?.data.gender && <div>
-                    <p className="text-white text-2xl my-2">Gender: <span className="text-white italic text-lg">{firebaseUser?.data?.gender}</span></p>
-                </div>}
-                {
-                    firebaseUser?.data.bio && <div>
-                        <h4 className="text-white text-2xl my-2">Bio</h4>
-                        <p>{firebaseUser?.data.bio}</p>
-                    </div>
-                }
-            </div>
-            <div className="flex justify-end mt-12">
-                <button className="flex items-center bg-transparent hover:border-gray-400 cursor-pointer rounded-xl text-white hover:text-gray-400 border-2 px-4 py-3 z-10" onClick={userSignOut}>Logout <AiOutlinePoweroff className="mx-1" size={18} />
+            <div className="flex justify-between mt-12">
+                <button className="flex items-center bg-white text-black cursor-pointer rounded-md text-lg border-2 px-6 py-3 z-10">Edit profile
+                </button>
+                <button className="flex items-center bg-transparent hover:border-gray-400 cursor-pointer rounded-md text-white hover:text-gray-500 text-lg border-2 px-6 py-3 z-10" onClick={userSignOut}>Logout
                 </button>
             </div>
         </div >
