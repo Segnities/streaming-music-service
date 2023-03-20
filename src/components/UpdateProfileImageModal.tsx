332fbf24fec54488abcbd666c09dd7b3226c44f0
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-03-20 18:46:28 +0200

    Update profile image, sync user info fields

---
+++ src/components/UpdateProfileImageModal.tsx
@@ -0,0 +1,54 @@
+import React, {useState} from "react";
+
+import {getStorage, uploadBytes, ref, getDownloadURL} from "firebase/storage";
+
+import Modal from "./UI/Modal";
+
+import {getAuth, updateProfile} from "firebase/auth";
+import {firebaseApp} from "../firebase/firebaseConfig";
+
+interface Props {
+    open: boolean;
+    setOpen:  React.Dispatch<React.SetStateAction<boolean>>;
+    photoURL:string;
+}
+
+const UpdateProfileImageModal = (props:Props) => {
+    const {open, setOpen, photoURL} = props;
+
+    const auth = getAuth(firebaseApp);
+    const storage = getStorage(firebaseApp);
+
+    const [profileImage, setProfileImage] = useState<File | null | undefined>(undefined);
+    const updateProfileImage = async () => {
+        const uploadPathRef = ref(storage, `profileImages/${profileImage?.name}`);
+
+        if (uploadPathRef !== undefined || uploadPathRef !== null) {
+            uploadBytes(uploadPathRef, profileImage!).then(res => console.log('Uploaded!')).catch(err => console.log('Upload error!')
+            );
+            const profileImageUrl = await getDownloadURL(ref(storage, `profileImages/${profileImage?.name}`));
+            updateProfile(auth.currentUser!, {
+                photoURL: profileImageUrl
+            }).then(res => console.log('Profile image updated!')).catch(err => console.log('Profile image update error'))
+
+        }
+        setOpen(false);
+    }
+
+    return (
+        <Modal open={open} setOpen={setOpen}>
+            <div className="flex flex-col items-center">
+                <img src={photoURL} className='w-1/5 h-1/5 rounded-full' alt=""/>
+                <label htmlFor="avatar" className='text-base font-medium mb-2'>Change profile
+                    avatar</label>
+                <input type="file" id="avatar"
+                       onChange={(e) => setProfileImage(e?.target?.files![0])}
+                       className="text-base normal-case my-1 outline-none line tracking-normal p-3 border-[1px] focus-visible:border-[3px]"/>
+                <button onClick={updateProfileImage} className='my-5 self-end bg-[#1ED760] rounded-xl w-3/6 md:w-1/6 text-2xl p-2 text-black font-medium'>Update</button>
+            </div>
+
+        </Modal>
+    );
+};
+
+export default UpdateProfileImageModal;
--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,50 +1,78 @@
-import { useContext, useState } from "react";
-import { useNavigate } from "react-router";
-import { useSelector } from "react-redux";
+import {useContext, useState, lazy, Suspense, useEffect} from "react";
+import {useNavigate} from "react-router";
+import {useSelector} from "react-redux";
 
+import {AuthContext, AuthType} from "../context";
 
-import { AuthContext, AuthType } from "../context";
+import {signOut, getAuth} from "firebase/auth"
+import {firebaseApp} from "../firebase/firebaseConfig";
 
-import { signOut, getAuth } from "firebase/auth"
-import { firebaseApp } from "../firebase/firebaseConfig";
-
-import { UserDoc } from "../utils/@types";
+import {UserDoc} from "../utils/@types";
 
 import BlockSpace from "../components/UI/BlockSpace/BlockSpace";
-import EditProfileModal from "../components/EditProfileModal";
-import { FirebaseUsersSelectorInterface } from "../store/reducers/firebaseUsers";
+
+const EditProfileModal = lazy(()=> import("../components/EditProfileModal"));
+const UpdateProfileImageModal = lazy(()=> import("../components/UpdateProfileImageModal"));
+
+import {FirebaseUsersSelectorInterface} from "../store/reducers/firebaseUsers";
+
+import Loader from "../components/UI/Loader";
 
 import NoImage from "../assets/no_artist.jpg";
-
 
 function User() {
     const authContext: AuthType | null = useContext(AuthContext);
     const navigate = useNavigate();
-    const { firebaseUsers: users } = useSelector((state: FirebaseUsersSelectorInterface) => state.firebaseUsers)
+
+    const {firebaseUsers: users} = useSelector((state: FirebaseUsersSelectorInterface) => state.firebaseUsers)
     const auth = getAuth(firebaseApp);
+
     const [firebaseUser, setFirebaseUser] = useState<UserDoc | undefined>(users.find(usr => usr.data.email === authContext?.user?.email));
-    const [openModal, setOpenModal] = useState<boolean>(false);
-    false
+    const [openEditProfile, setOpenEditProfile] = useState<boolean>(false);
+    const [openUpdateProfileImage, setOpenUpdateProfileImage] = useState<boolean>(false);
+
+    const [photoURL, setPhotoURL] = useState('');
+
     const userSignOut = () => {
         signOut(auth);
         authContext?.setUser(null);
         navigate("/");
     }
 
+    useEffect(()=> {
+        const userProfileImage:string | undefined | null= auth?.currentUser?.photoURL as string;
+        setPhotoURL(userProfileImage !== null && !userProfileImage?.includes('undefined') ? userProfileImage : NoImage);
+    }, []);
+
     return (
         <div className="flex flex-col w-full">
-            <EditProfileModal avatar={authContext?.user?.photoURL ?? NoImage} firebaseUser={firebaseUser}
-                setOpenModal={setOpenModal} openModal={openModal} />
+            <Suspense fallback={<Loader title={'Profile is loading...'}/>}>
+                <EditProfileModal
+                    photoURL={photoURL}
+                    firebaseUser={firebaseUser}
+                    openModal={openEditProfile}
+                    setOpenModal={setOpenEditProfile}
+                    setFirebaseUser={setFirebaseUser}
+                />
+            </Suspense>
+            <Suspense fallback={<Loader title={'Profile image modal is loading...'}/>}>
+                <UpdateProfileImageModal
+                    open={openUpdateProfileImage}
+                    setOpen={setOpenUpdateProfileImage}
+                    photoURL={photoURL}
+                />
+            </Suspense>
             <h1 className="text-2xl sm:text-3xl text-white font-semibold my-5">Account overview</h1>
             <article className="relative w-full flex flex-col">
                 <h2 className="text-xl sm:text-2xl text-white font-medium my-4">Profile</h2>
                 <div className="w-full h-28 bg-gradient-to-l from-transparent to-black sm:h-52"></div>
                 <section className="absolute inset-0 flex flex-row items-center">
                     <img
-                        src={authContext?.user?.photoURL ?? NoImage}
+                        src={photoURL}
                         alt="art"
                         className="w-28 sm:w-48 h-28 sm:h-48 rounded-full object-cover border-2 shadow-xl shadow-black cursor-pointer"
                         onDragStart={(e) => e.preventDefault()}
+                        onClick={()=> setOpenUpdateProfileImage(true)}
                         title="Click to edit profile"
                     />
 
@@ -52,37 +80,37 @@ function User() {
                         {authContext?.user?.displayName ?? firebaseUser?.data?.username}
                     </h3>
                 </section>
-                <BlockSpace />
+                <BlockSpace/>
             </article>
             <div className="flex flex-col">
                 <table className="w-full">
                     <colgroup>
-                        <col className="w-1/2" />
-                        <col className="w-1/2" />
+                        <col className="w-1/2"/>
+                        <col className="w-1/2"/>
                     </colgroup>
                     <tbody>
-                        <tr className="border-b-2 border-[#dedede]">
-                            <td className="py-5 text-gray-500 text-lg">Username</td>
-                            <td className="py-5 text-white text-lg">{firebaseUser?.data?.username ?? authContext?.user?.displayName}</td>
-                        </tr>
-                        <tr className="border-b-2 border-[#dedede]">
-                            <td className="py-5 text-gray-500 text-lg">Email</td>
-                            <td className="py-5 text-white text-lg">{firebaseUser?.data?.email ?? authContext?.user?.email}</td>
-                        </tr>
-                        <tr className="border-b-2 border-[#dedede]">
-                            <td className="py-5 text-gray-500 text-lg">Date of birth</td>
-                            <td className="py-5 text-white text-lg">{firebaseUser?.data?.birthday || "Unknown"}</td>
-                        </tr>
-                        <tr className="border-b-2 border-[#dedede]">
-                            <td className="py-5 text-gray-500 text-lg">Gender</td>
-                            <td className="py-5 text-white text-lg">{firebaseUser?.data?.gender || "Unknown"}</td>
-                        </tr>
+                    <tr className="border-b-2 border-[#dedede]">
+                        <td className="py-5 text-gray-500 text-lg">Username</td>
+                        <td className="py-5 text-white text-lg">{firebaseUser?.data?.username ?? authContext?.user?.displayName}</td>
+                    </tr>
+                    <tr className="border-b-2 border-[#dedede]">
+                        <td className="py-5 text-gray-500 text-lg">Email</td>
+                        <td className="py-5 text-white text-lg">{firebaseUser?.data?.email ?? authContext?.user?.email}</td>
+                    </tr>
+                    <tr className="border-b-2 border-[#dedede]">
+                        <td className="py-5 text-gray-500 text-lg">Date of birth</td>
+                        <td className="py-5 text-white text-lg">{firebaseUser?.data?.birthday || "Unknown"}</td>
+                    </tr>
+                    <tr className="border-b-2 border-[#dedede]">
+                        <td className="py-5 text-gray-500 text-lg">Gender</td>
+                        <td className="py-5 text-white text-lg">{firebaseUser?.data?.gender || "Unknown"}</td>
+                    </tr>
                     </tbody>
                 </table>
             </div>
             <div className="flex justify-between mt-12">
-                <button onClick={() => setOpenModal(true)}
-                    className="flex items-center bg-white text-black cursor-pointer rounded-md text-lg border-2 px-6 py-3 z-10">Edit
+                <button onClick={() => setOpenEditProfile(true)}
+                        className="flex items-center bg-white text-black cursor-pointer rounded-md text-lg border-2 px-6 py-3 z-10">Edit
                     profile
                 </button>
                 <button
