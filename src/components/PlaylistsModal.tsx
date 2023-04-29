9137bfc2114759e1953e0dff5cf0b4a62746db98
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-04-29 18:24:08 +0300

    nanoid, managePlaylistsSongs

--- src/components/PlaylistsModal.tsx
+++ src/components/PlaylistsModal.tsx
@@ -1,25 +1,72 @@
+import { useState, useEffect } from "react";
+
+import { useFormik } from "formik";
+
+import { createPlaylistValidationSchema } from "../validation";
+
 import { ModalSm } from "./UI/Modal";
 
 import { IoMdAdd } from "react-icons/io";
+import { useGetCurrentUser } from "../hooks/useGetCurrentUser";
+import { DocumentData, collection, getDocs, query, where } from "firebase/firestore";
+import { firebaseDatabase } from "../firebase/firebaseConfig";
+
+
 
 interface Props {
     openPlaylistModal: boolean;
     setOpenPlaylistModal: React.Dispatch<React.SetStateAction<boolean>>;
+    managePlayslistsSongs: (playlist_title: string, playlist_id: string) => Promise<void>;
 }
 
-export default function PlaylistsModal({ openPlaylistModal, setOpenPlaylistModal, ...props }: Props) {
+export default function PlaylistsModal({ openPlaylistModal, setOpenPlaylistModal, managePlayslistsSongs, ...props }: Props) {
+    const [playlists, setPlaylists] = useState<DocumentData[] | null>(null);
+    const [user, firebaseUser] = useGetCurrentUser();
+
+    const playlists_collection = collection(firebaseDatabase, "users_playlists");
+
+
+    const formik = useFormik({
+        initialValues: {
+            playlistTitle: ""
+        },
+        onSubmit: values => {
+            managePlayslistsSongs(values.playlistTitle, "");
+        },
+        validationSchema: createPlaylistValidationSchema
+    });
+
+    const getPlaylists = async () => {
+        const q = query(playlists_collection, where("uid", "==", firebaseUser.id));
+        const querySnapshot = await getDocs(q);
+        const _playlists: DocumentData[] = [];
+
+        querySnapshot.forEach((doc) => {
+            _playlists.push(doc.data().playlists);
+        });
+
+        setPlaylists(_playlists);
+    };
+
+    useEffect(() => {
+        getPlaylists();
+    }, [playlists_collection])
+
+
     return (
         <>
             {
+
                 openPlaylistModal && (
                     <ModalSm
                         open={openPlaylistModal}
                         setOpen={setOpenPlaylistModal}
                         overscroll={false}
                     >
-                        <form onSubmit={(e) => e.preventDefault()} className="w-full flex flex-row flex-auto justify-between items-center">
-                            <input type="text" className="w-full p-3 border-black border-b-2 focus:outline-none" />
-                            <IoMdAdd className="cursor-pointer" role="button" size={24} />
+
+                        <form onSubmit={formik.handleSubmit} className="w-full flex flex-row flex-auto justify-between items-center">
+                            <input type="text" id="playlistTitle" name="playlistTitle" value={formik.values.playlistTitle} onChange={formik.handleChange} className="w-full p-3 border-black border-b-2 focus:outline-none" title={formik.errors.playlistTitle ?? "Enter playlist title"} />
+                            <IoMdAdd className="cursor-pointer" role="button" type="submit" size={24} />
                         </form>
                     </ModalSm>
                 )
