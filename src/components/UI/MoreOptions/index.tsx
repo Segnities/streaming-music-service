7d45f0cf0fc32a75305d6579a0e8789184c6f9d1
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-05-18 09:55:36 +0300

    Close MoreActionsGroup in an area outside of this component

--- src/components/UI/MoreOptions/index.tsx
+++ src/components/UI/MoreOptions/index.tsx
@@ -4,10 +4,6 @@ import LineDivider from "../LineDivider";
 
 import { BsThreeDots } from "react-icons/bs";
 
-type OptionIcon = {
-    title: string;
-    icon: JSX.Element;
-}
 
 type Option = {
     key: string,
@@ -30,6 +26,8 @@ interface MoreOptionsIconProps {
 
 interface MoreActionsListProps {
     options: Option[];
+    showMore: boolean;
+    setShowMore: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 interface MoreActionsGroupProps {
@@ -62,7 +60,7 @@ export function MoreActionsGroup(props: MoreActionsGroupProps) {
 
             {
                 user?.uid && (
-                    <MoreActionsList options={optionsList} />
+                    <MoreActionsList options={optionsList} showMore={showMore} setShowMore={setShowMore} />
 
                 )
             }
@@ -72,29 +70,41 @@ export function MoreActionsGroup(props: MoreActionsGroupProps) {
 }
 
 export function MoreActionsList(props: MoreActionsListProps) {
+    const rootClass = `${props.showMore ? "flex justify-center items-center fixed inset-0 z-[30]" : "hidden"}`;
+
     return (
-        <section className="block md:hidden my-5">
-            <h5 className="text-white font-bold my-3 text-2xl">Actions:</h5>
-            <ul className="flex flex-col items-start justify-center">
-                {
-                    props.options.map((option) => (
-                        <li
-                            key={option.key}
-                            className="w-full h-12 flex items-center justify-center my-2 bg-[#4c426e] rounded-md focus-within:bg-transparent border-2 border-transparent focus-within:border-white focus-within:transition-colors focus-within:ease-out focus-within:duration-100">
-                            <button className="text-white w-full h-full uppercase font-bold text-sm rounded-xl"
-                                onClick={() => option.onClickCallback()}
-                            >{option.title}</button>
-                        </li>
-                    ))
-                }
-            </ul>
-        </section>
+        <div className={rootClass}
+            id="more-actions-group"
+            onClick={(e) => {
+                props.setShowMore(false);
+                e.stopPropagation();
+            }}
+        >
+            <section className="block md:hidden my-5"
+
+            >
+                <h5 className="text-white font-bold my-3 text-2xl">Actions:</h5>
+                <ul className="flex flex-col items-start justify-center">
+                    {
+                        props.options.map((option) => (
+                            <li
+                                key={option.key}
+                                className="w-full h-12 flex items-center justify-center my-2 bg-[#4c426e] rounded-md focus-within:bg-transparent border-2 border-transparent focus-within:border-white focus-within:transition-colors focus-within:ease-out focus-within:duration-100">
+                                <button className="text-white w-full h-full uppercase font-bold text-sm rounded-xl"
+                                    onClick={() => option.onClickCallback()}
+                                >{option.title}</button>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </section>
+        </div>
     );
 }
 
 export function MoreOptionsIcon({ user, setShowMore, showMore }: MoreOptionsIconProps) {
     return (
-        <div className="hidden md:block z-30">
+        <div className={`hidden md:block z-[2000]`}>
             {
                 user?.uid && (
                     <BsThreeDots size={32} className="cursor-pointer" color="white" onClick={() => setShowMore(!showMore)} />
@@ -112,16 +122,18 @@ export function MoreOptions(props: MoreOptionProps) {
             {
                 props.visible && (
                     <div className="hidden absolute top-10 duration-200 ease-linear transition-all md:flex flex-col items-start justify-start rounded-sm p-1 min-h-[150px] w-72 bg-[#21106e] z-50">
-                        {
-                            props.options.map((option) => (
-                                <p
-                                    key={option.key}
-                                    onClick={() => option.onClickCallback()}
-                                    className="w-full text-start flex items-center font-semibold first-letter:uppercase cursor-default text-sm max-h-12 justify-start px-4 py-2 hover:rounded-md  hover:border-[#7b64e2] hover:text-[#7b64e2] hover:bg-[#110642]  my-1 text-white">
-                                    {option.title}
-                                </p>
-                            ))
-                        }
+                        <ul className="w-full h-full">
+                            {
+                                props.options.map((option) => (
+                                    <li
+                                        key={option.key}
+                                        onClick={() => option.onClickCallback()}
+                                        className="w-full text-start flex items-center font-semibold first-letter:uppercase cursor-default text-sm max-h-12 justify-start px-4 py-2 hover:rounded-sm  hover:border-[#7b64e2] hover:text-[#7b64e2] hover:bg-[#110642] my-1 text-white">
+                                        {option.title}
+                                    </li>
+                                ))
+                            }
+                        </ul>
                     </div>
                 )}
         </>
