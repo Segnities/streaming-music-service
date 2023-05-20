8f07e4143e9ea9ce09ae0795a40150f0db8c6742
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2023-05-20 09:38:59 +0300

    Playlist songs

--- src/components/SongCard.tsx
+++ src/components/SongCard.tsx
@@ -13,16 +13,16 @@ interface Props {
   index: number;
   activeSong: SongRootObject;
   isPlaying: boolean;
-  data: [] | SongRootObject[];
+  songs: [] | SongRootObject[];
   children?: JSX.Element;
 }
 
 function SongCard(props: Props) {
-  const { song, index, activeSong, isPlaying, data } = props;
+  const { song, index, activeSong, isPlaying, songs } = props;
   const dispatch = useDispatch();
 
   const handlePlayClick = () => {
-    dispatch(setActiveSong({ song, data, index }));
+    dispatch(setActiveSong({ song, songs, index }));
     dispatch(playPause(true));
   };
 
@@ -32,9 +32,9 @@ function SongCard(props: Props) {
 
   return (
     <div className="flex flex-col w-[150px] h-[240px] sm:w-[220px] lg:w-[230px] lg:h-[300px] p-4 bg-white/5 bg-opacity bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
-      <div className="relative w-full h-56 group">
+      <div className="relative w-full h-56 group bg-transparent z-10">
         <div
-          className={`absolute inset-0 justify-center items-center bg-black/20 rounded-lg bg-opacity-50 group-hover:flex ${activeSong?.title === song.title
+          className={`absolute  inset-0 justify-center items-center bg-black/20 rounded-lg bg-opacity-50 group-hover:flex ${activeSong?.title === song.title
             ? "flex bg-black bg-opacity-70"
             : "hidden"
             }`}
--- src/pages/MyPlaylist.tsx
+++ src/pages/MyPlaylist.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
 
 import BgDivider from "../components/UI/BgDivider/BgDivider";
@@ -8,11 +8,24 @@ import { useGetCurrentUser } from "../hooks/useGetCurrentUser";
 import getMyPlaylistById from "../helpers/getMyPlaylistById";
 import { Playlist } from "../types/playlist";
 
+import AbsoluteFlexWrapper from "../components/UI/Wrapper/AbsoluteFlexWrapper";
+import PlaylistImage from "../components/UI/Img/PlaylistImage";
+import SongCard from "../components/SongCard";
+
+import { SelectorPlayerState } from "../API/types";
+import BlockSpace from "../components/UI/BlockSpace/BlockSpace";
+
 export default function MyPlaylist() {
     const { playlist_id } = useParams();
     const { firebaseUser } = useGetCurrentUser();
 
+    const dispatch = useDispatch();
+
     const [playlist, setPlaylist] = useState<Playlist | undefined>(undefined);
+
+    const { activeSong, isPlaying, genreListId } = useSelector(
+        (state: SelectorPlayerState) => state.player
+    );
 
     useEffect(() => {
         getMyPlaylistById(playlist_id, firebaseUser?.id).then((plst) => {
@@ -26,14 +39,38 @@ export default function MyPlaylist() {
     return (
         <div className="grid grid-flow-row">
             <div className="relative w-full flex flex-col">
-                <BgDivider/>
-                
-                <h1 className="text-white text-3xl font-bold text-underline">
-                    {playlist?.title ?? "My Playlist"}
-                </h1>
-                <p className="text-white text-xl">{playlist?.description ?? "No description"}</p>
+                <BgDivider />
+                <AbsoluteFlexWrapper>
+                    <PlaylistImage
+                        iconColor="white"
+                        iconSize={42}
+                        tailwindWidth="w-28 sm:w-48"
+                        tailwindHeight="h-28 sm:h-48"
+                        tailwindBg="bg-gradient-to-br from-violet-500 to-[#121286] bg-opacity bg-opacity-80 backdrop-blur-md"
+                        preventHoverEffect />
+                    <div className="ml-5">
+                        <h1 className="text-white text-3xl font-bold text-underline">
+                            {playlist?.title ?? "My Playlist"}
+                        </h1>
+                        <p className="text-white text-sm">{playlist?.description ?? "No description"}</p>
+                    </div>
+                </AbsoluteFlexWrapper>
             </div>
-           
+            <BlockSpace />
+            <div className="flex flex-row flex-wrap gap-5">
+                {
+                    playlist?.songs.map((song, index) => (
+                        <SongCard
+                            key={song.key}
+                            index={index}
+                            activeSong={activeSong}
+                            song={song}
+                            isPlaying={isPlaying}
+                            songs={playlist.songs}
+                        />
+                    ))
+                }
+            </div>
         </div>
     );
 }
